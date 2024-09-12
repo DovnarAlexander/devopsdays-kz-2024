1ebc56a47a6f5495969cc68052b690d0805df56b
DovnarAlexander <dovnaralexander@users.noreply.github.com>
2024-09-12 21:44:20 +0300

    added meterials [skip ci]

---
+++ scripts/gc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e -x
+
+git checkout main
+git pull
+git checkout gh-pages
+git push --force
+
+gh release list --limit 500 \
+  | cut -f3 \
+  | while read release_tag; do
+  gh release delete -y "$release_tag"
+done
+
+git fetch
+git tag -l | xargs -n 1 git push --delete origin
+git tag -l | xargs git tag -d
+
+gh run list -L 300 --json databaseId  -q '.[].databaseId' | xargs -IID gh api \
+    "repos/$(gh repo view --json nameWithOwner -q .nameWithOwner)/actions/runs/ID" \
+    -X DELETE --silent
